63cb847abcc84a230d8bcea29a36710572ce909a
bcolloran <brendan@colloran.net>
2023-07-16 16:42:42 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use bevy::prelude::*;
+
+#[cfg(not(feature = "reload"))]
+use systems::*;
+#[cfg(feature = "reload")]
+use systems_hot::*;
+
+#[cfg(feature = "reload")]
+#[hot_lib_reloader::hot_module(dylib = "systems")]
+mod systems_hot {
+    use bevy::prelude::*;
+    pub use components::*;
+    hot_functions_from_file!("systems/src/lib.rs");
+}
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(
+            Update,
+            (
+                player_movement_system,
+                player_shooting_system,
+                bullet_movement_system,
+                bullet_hit_system,
+                spawn_other_ships,
+                move_other_ships,
+            ),
+        )
+        .add_systems(Update, bevy::window::close_on_esc);
+
+    app.run();
+}
